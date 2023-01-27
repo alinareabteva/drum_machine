d1836ff6b0794a95aed22f4dc9cd2c72b23b312f
alinareabteva <52407474+alinareabteva@users.noreply.github.com>
2023-01-27 12:32:38 +0200

    Migrated from CodePen

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import DrumMachine from './drum_machine/DrumMachine';
+import './App.css';
+
+function App() {
+  return (
+    <DrumMachine />
+  );
+}
+
+export default App;
---
+++ src/drum_machine/constants.js
@@ -0,0 +1,65 @@
+export const PADS = [
+    {
+      name: 'Heater 1',
+      source: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
+      type: 'audio/mpeg',
+      title: 'Q',
+      color: '#f94144'
+    },
+    {
+      name: 'Heater 2',
+      source: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
+      type: 'audio/mpeg',
+      title: 'W',
+      color: '#f3722c'
+    },
+    {
+      name: 'Heater 3',
+      source: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
+      type: 'audio/mpeg',
+      title: 'E',
+      color: '#f9c74f'
+    },
+    {
+      name: 'Heater 4',
+      source: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3',
+      type: 'audio/mpeg',
+      title: 'A',
+      color: '#2fbf71'
+    },
+    {
+      name: 'Clap',
+      source: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',
+      type: 'audio/mpeg',
+      title: 'S',
+      color: '#27a300'
+    },
+    {
+      name: 'Open-HH',
+      source: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
+      type: 'audio/mpeg',
+      title: 'D',
+      color: '#168aad'
+    },
+    {
+      name: 'Kick-n-Hat',
+      source: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
+      type: 'audio/mpeg',
+      title: 'Z',
+      color: '#758bfd'
+    },
+    {
+      name: 'Kick',
+      source: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
+      type: 'audio/mpeg',
+      title: 'X',
+      color: '#9381ff'
+    },
+    {
+      name: 'Closed-HH',
+      source: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
+      type: 'audio/mpeg',
+      title: 'C',
+      color: '#7b2cbf'
+    }
+  ]
